20e296ae6630ce7328193cedb68264e769293c47
sweetpoisonxx <avilsu111@gmail.com>
2023-06-28 10:33:08 +0530

    2 commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Home from './home/Home'
+import './app.scss';
+
+function App() {
+  return (
+    <div>
+      <Home/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/home/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div>
+      HEllo world
+      
+    </div>
+  )
+}
+
+export default Home
