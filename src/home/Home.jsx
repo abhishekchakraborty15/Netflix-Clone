74be14daa60a14e307df96a9981332fface4afa2
sweetpoisonxx <avilsu111@gmail.com>
2023-06-28 10:45:57 +0530

    4th commit

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import '../app.scss';
+import '../home/home.scss';
 
 const Home = () => {
   return (
-    <div>
-      HEllo world
-      
+    <div className='home'>
+      HEllo worldd
     </div>
   )
 }
